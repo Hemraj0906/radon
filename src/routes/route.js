aa74da4ac62cfbb3fe560ca69cf0a89568fb4105
Hemraj0906 <hemrajdeshmukh0906@gmail.com>
2022-06-14 21:11:12 -0400

    assignmentolvingJWToken

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const userController= require("../controllers/userController")
+const comMiddleware= require("../Middleware/auth")
 
-const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
-const publisherController=require("../controllers/publisherController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createAuthor", authorController.createAuthor  )
+router.post("/users",userController.createUser  )
 
-router.post("/createPublisher", publisherController.createPublisher)
-router.post("/createBook", bookController.createBook  )
-router.get("/getBooksData", bookController.getBooksData)
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
-router.get("/getAuthorIdBook",bookController.getAuthorIdBook)
+router.post("/login", userController.loginUser)
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+//The userId is sent by front end
+router.get("/users/:userId",comMiddleware.mid,userController.getUserData)
 
-
-
-
-
+router.put("/users/:userId", userController.updateUser)
+router.delete("/users/:userId",userController.isDelete)
 
 module.exports = router;
